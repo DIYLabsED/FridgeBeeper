472539034b8811ffd1d7fb08049ace827ac663e7
DIYLabsED <146977205+DIYLabsED@users.noreply.github.com>
2025-01-28 11:38:11 +0530

    Timing logic for three beeps

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,31 @@
-#include "Arduino.h"
-#include "avr/sleep.h"       // sleep
-#include "avr/interrupt.h"   // wake up
+#include "Arduino.h"                    
+#include "avr/sleep.h"                   // sleep
+#include "avr/interrupt.h"               // wake up
 
 const int BUZZER_PIN = 4;
-const int SENSOR_PIN = 2;    // Not expected to be changed, PB2 is hard-coded when setting up registers
+const int SENSOR_PIN = 2;                // not expected to be changed, PB2 is hard-coded when setting up registers
 
-volatile bool sensorState = true; // true = door open, false = door closed
+volatile bool sensorState = true;        // true = door open, false = door closed
 
-void beep1(); // *click*
-void beep2(); // *beep*
-void beep3(); // *BEEP BEEP*
+const uint16_t STAGE1_INTERVAL = 60000;  // 1 minute
+const uint16_t STAGE2_INTERVAL = 60000; // 5 minutes
+int beepState = 0;
+uint16_t prevMillis = 0;
+
+void beep1();                            // *click*
+void beep2();                            // *beep*
+void beep3();                            // *BEEP BEEP*
+void handleBeep();
+
 
 void setup(){
 
     pinMode(BUZZER_PIN, OUTPUT);
     pinMode(SENSOR_PIN, INPUT_PULLUP);    
 
-    GIMSK |= (1 << PCIE);                 // Enable pin change interrupt, General Interrupt Mask
-    PCMSK |= (1 << PCINT2);               // Enable pin change interrupt on PB2, Pin Change Mask
-    sei();                                // Enable global interrupts
+    GIMSK |= (1 << PCIE);                // enable pin change interrupt, General Interrupt Mask
+    PCMSK |= (1 << PCINT2);              // enable pin change interrupt on PB2, Pin Change Mask
+    sei();                               // enable global interrupts
 
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 
@@ -27,11 +34,13 @@ void setup(){
 void loop(){
     
     if(sensorState){
-        beep1();    
+
+      handleBeep();
+
     }
 
     else{
-        sleep_mode();        
+        sleep_mode();                    // sleeping conveniently resets millis()    
     }
 
 }
@@ -74,5 +83,38 @@ void beep3(){
     digitalWrite(BUZZER_PIN, LOW);
 
     delay(1000);
+
+}
+
+void handleBeep(){
+
+  switch (beepState) {
+    case 0:
+      if (millis() - prevMillis >= STAGE1_INTERVAL) {
+        beepState = 1; // Update state to 1 after interval1
+        prevMillis = millis(); // Reset the reference time
+      } else {
+        beep1();
+      }
+      break;
+
+    case 1:
+      if (millis() - prevMillis >= STAGE2_INTERVAL) {
+        beepState = 2; // Update state to 2 after interval2
+        prevMillis = millis(); // Reset the reference time
+      } else {
+        beep2();
+      }
+      break;
+
+    case 2:
+      beep3();
+      // No need to update state as beep3() is the final function
+      break;
+
+    default:
+      // Do nothing
+      break;
+  }
 
 }
